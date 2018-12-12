0375458bead745f4eba11e4fe3a8a91cc7d37ac8
Ben WU <wubin28@gmail.com>
2018-12-12 08:22:25 +0800

    java based configuration - import a java config class

--- SpringBaseball/src/main/java/com/oreilly/config/AppConfig.java
+++ SpringBaseball/src/main/java/com/oreilly/config/AppConfig.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Set;
 
 @Configuration
+@Import(InfrastructureConfig.class)
 @ComponentScan(basePackages = "com.oreilly")
 @EnableAspectJAutoProxy
 public class AppConfig {
